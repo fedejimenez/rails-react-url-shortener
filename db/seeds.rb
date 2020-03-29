1da859bc4c0b43c7a61e1b73474218ecbbea14ab
Federico Jimenez <fedeejimenez@gmail.com>
2020-03-29 00:03:49 +0000

    Use pry gem and add seed for first url

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Url.create(long_url: "https://www.fakelongurl.com", short_url: "https://sho.rt/123456")
